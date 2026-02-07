325c28d2694cb417585ba2025bf8d0180b52ad04
Fernando Chavarria <202101815@udv.edu.gt>
2026-02-07 10:39:55 -0600

    chore: update labels and placeholders in login form

--- Proyecto-login/src/app/components/login/login.component.ts
+++ Proyecto-login/src/app/components/login/login.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  form;
+  constructor(private fb: FormBuilder){
+    this.form = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }  
+  
+  submit() {
+    if (this.form.invalid) {
+      console.error('Formulario inválido', this.form.value);
+      return;
+    }
+    console.log('Login OK', this.form.value);
+  }
 }
